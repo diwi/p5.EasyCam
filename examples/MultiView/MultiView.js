63e3bf12a8cafa887c35eae14f1e426929e48f33
Thomas Diewald <diewald31415@gmail.com>
2018-01-01 20:39:48 +0100

    MultiView Example

--- examples/MultiView/MultiView.js
+++ examples/MultiView/MultiView.js
@@ -26,14 +26,10 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   
   setAttributes('antialias', true);
-  console.log(Dw.EasyCam.INFO);
   
   var RENDERER = this._renderer;
-  // console.log(this);
-  // console.log(this._renderer);
-  // console.log(this._renderer._pInst);
-
-  // console.log(this instanceof p5);
+  
+  console.log(Dw.EasyCam.INFO);
 
   cameras.length = NX * NY;
  
@@ -104,29 +100,9 @@ function windowResized() {
 }
 
 
-function setGLGraphicsViewport(x,y,w,h){
-  var gl = this._renderer.GL;
-  gl.enable(gl.SCISSOR_TEST);
-  gl.scissor (x,y,w,h);
-  gl.viewport(x,y,w,h);
-}
-
 
 function draw(){
-  // for (var i in cameras){
-    // cameras[i].update();
-  // }
-  
-  // cameras.forEach(function(cam) { 
-    // cam.update();
-  // });
-  
-  
-  // cameras.forEach(function(cam) { 
-    // displayScene(cam);
-  // });
-  
-  
+
   // update current camera states
   for (var i in cameras){
     cameras[i].update();
@@ -139,6 +115,8 @@ function draw(){
   setGLGraphicsViewport(0,0,width,height);
   background(0);
   
+  randomSeed(0);
+  
   // render scene once per camera/viewport
   for (var i in cameras){
     var cam =  cameras[i];
@@ -147,6 +125,14 @@ function draw(){
     pop();
   }
   
+}
+
+
+function setGLGraphicsViewport(x,y,w,h){
+  var gl = this._renderer.GL;
+  gl.enable(gl.SCISSOR_TEST);
+  gl.scissor (x,y,w,h);
+  gl.viewport(x,y,w,h);
 }
 
 
@@ -164,32 +150,138 @@ function displayScene(cam){
   setGLGraphicsViewport(x,y_inv,w,h);
   
   // modelview - using camera state
-  cam.apply(this._renderer);
+  cam.apply(this);
   
   // projection - using camera viewport
   perspective(60 * PI/180, w/h, 1, 5000);
   
   // clear background (scissors makes sure we only clear the region we own)
-  background(32);
+  background(255);
   
   // render scene as usual
 	strokeWeight(1);
 
-	fill(255, 64, 0);
-	box(200);
+	// fill(255, 64, 0);
+	// box(200);
   
-	push();
-	translate(0, +200, 0);
-	fill(0, 64, 255);
-	box(100);
-	pop();
+	// push();
+	// translate(0, +200, 0);
+	// fill(0, 64, 255);
+	// box(100);
+	// pop();
+  
+  // push();
+	// translate(0, -200, 0);
+	// fill(64, 255, 0);
+	// box(50);
+	// pop();
+  
+  var boxes_x = 3;
+  var boxes_y = 3;
+  
+  var dimx = 100;
+  var dimy = dimx;
+  var dimz = (dimx + dimy) * 0.2;
+  
+  var gridx = dimx * 1.2;
+  var gridx = dimy * 1.2;
+  var size_x = boxes_x * gridx;
+  var size_y = boxes_y * gridx;
+  
+  if(cam.ID == 0){
+    background(16);
+    ambientLight(100);
+    pointLight(255, 255, 255, 100, 0, 100);
+    noStroke();
+  }
+  if(cam.ID == 1){
+    background(16);
+    ambientLight(100);
+    pointLight(255, 200, 200, 100, 0, 100);
+    noStroke();
+  }
+  if(cam.ID == 2){
+    background(16);
+  }
+  
+
+  // just some stuff to get a color, playing around here
+  var rsin = (sin(cam.ID  + frameCount * 0.01) * 0.5 + 0.5);
+  
+  var palette = palette_list[1];
+  var rgba, cr, cg, cb, idx, tx, ty, tz, ii, inorm;
   
   push();
-	translate(0, -200, 0);
-	fill(64, 255, 0);
-	box(50);
-	pop();
+  translate(-size_x/2, -size_y/2, 0);
+  for(var iy = 0; iy < boxes_y; iy++){
+    for(var ix = 0; ix < boxes_x; ix++){
+      
+      ii = (iy * boxes_y + ix);
+      inorm = ii / (boxes_x * boxes_y - 1) * rsin;
+          
+      // position       
+      tx = (ix + 0.5) * gridx;
+      ty = (iy + 0.5) * gridx;
+      tz = 0;
+       
+      // color
+      rgba = getColor(inorm, palette, rgba);
+      cr = rgba[0] * 255;
+      cg = rgba[1] * 255;
+      cb = rgba[2] * 255;
+      
+      var gs = (cr + cg + cb) / 3;
+      
+      switch(cam.ID){
+        case 0:  ambientMaterial(cr, cg, cb);break;
+        case 1:  ambientMaterial(gs, gs, gs);break;
+        case 2:  fill(255)                  ;break;
+        default: fill(cr, cg, cb)           ; break;
+      }
+      
+      push();
+      translate(tx, ty, tz);
+      if(ix == 2 && iy == 2){
+        torus(dimx/2 - dimz/2, dimz/2, 24, 16);
+
+      } else {
+        box(dimx, dimy, dimz);
+      }
+      pop();
+    }
+  }
+  pop();
+
+}
+
+
+
+
+
+// http://www.iquilezles.org/www/articles/palettes/palettes.htm
+var palette_list = [
+  [ [0.5, 0.5, 0.5, 1.0],   [0.5, 0.5, 0.5, 0.0],   [1.0, 1.0, 1.0, 1.0],   [0.90, 0.40, 0.20, 1.0] ],
+  [ [0.5, 0.5, 0.5, 1.0],   [0.5, 0.5, 0.5, 0.0],   [1.0, 1.0, 1.0, 1.0],   [0.00, 0.10, 0.20, 1.0] ],
+  [ [0.5, 0.5, 0.5, 1.0],   [0.5, 0.5, 0.5, 0.0],   [1.0, 1.0, 1.0, 1.0],   [0.30, 0.20, 0.20, 1.0] ],
+  [ [0.5, 0.5, 0.5, 1.0],   [0.5, 0.5, 0.5, 0.0],   [1.0, 1.0, 0.5, 1.0],   [0.80, 0.90, 0.30, 1.0] ],
+  [ [0.5, 0.5, 0.5, 1.0],   [0.5, 0.5, 0.5, 0.0],   [1.0, 0.7, 0.4, 1.0],   [0.00, 0.15, 0.20, 1.0] ],
+  [ [0.5, 0.5, 0.5, 1.0],   [0.5, 0.5, 0.5, 0.0],   [2.0, 1.0, 0.0, 1.0],   [0.50, 0.20, 0.25, 1.0] ],
+  [ [0.8, 0.5, 0.4, 1.0],   [0.2, 0.4, 0.2, 0.0],   [2.0, 1.0, 1.0, 1.0],   [0.00, 0.25, 0.25, 1.0] ],
+];
+
+function getColor(t, palette, dst){
+  dst = ((dst === undefined) || (dst.constructor !== Array)) ? [0, 0, 0, 0] : dst;
   
+  dst[0] = colFunc(t, palette[0][0], palette[1][0], palette[2][0], palette[3][0]);
+  dst[1] = colFunc(t, palette[0][1], palette[1][1], palette[2][1], palette[3][1]);
+  dst[2] = colFunc(t, palette[0][2], palette[1][2], palette[2][2], palette[3][2]);
+  dst[3] = colFunc(t, palette[0][3], palette[1][3], palette[2][3], palette[3][3]);
+  
+  return dst;
+}
+
+function colFunc(t, a, b, c, d){
+  return a + b * Math.cos(Math.PI * 2 * (c * t + d));
 }
 
 

3ca874d62aec88930f2a9deb895c0fd5c5a0aa9a
Thomas Diewald <diewald31415@gmail.com>
2018-01-14 21:27:15 +0100

    performance fix for p5

--- examples/ReactionDiffusion_Webgl2/ReactionDiffusion_Webgl2.js
+++ examples/ReactionDiffusion_Webgl2/ReactionDiffusion_Webgl2.js
@@ -51,7 +51,7 @@ var shader_grayscott;
 var shader_display;
 
 // offscreen resolution scale factor.
-var SCREEN_SCALE = 0.5; 
+var SCREEN_SCALE = 1.0; 
 
 // reaction diffusion settings and presets
 var rdDef = {
@@ -69,7 +69,8 @@ var rdDef = {
   preset3 : function() {  this.feed = 0.015; this.kill = 0.050; this.da = 1.00; this.db = 0.60; },
   preset4 : function() {  this.feed = 0.072; this.kill = 0.062; this.da = 0.50; this.db = 0.25; },
   preset5 : function() {  this.feed = 0.071; this.kill = 0.063; this.da = 0.40; this.db = 0.20; },
-
+  preset6 : function() {  this.feed = 0.023; this.kill = 0.052; this.da = 0.50; this.db = 0.50; },
+  preset7 : function() {  this.feed = 0.029; this.kill = 0.056; this.da = 0.60; this.db = 0.46; },
 };
 
 
@@ -95,6 +96,8 @@ function setup() {
   gui.add(rdDef, 'preset3');
   gui.add(rdDef, 'preset4');
   gui.add(rdDef, 'preset5');
+  gui.add(rdDef, 'preset6');
+  gui.add(rdDef, 'preset7');
   gui.add(rdDef, 'reset'  );
   
   
@@ -152,6 +155,7 @@ function setup() {
  
   // place initial samples
   initRD();
+ 
 }
 
 
@@ -227,7 +231,6 @@ function draw(){
   shader_display.uniformF('wh_rcp', [1.0/w, 1.0/h]);
   shader_display.quad();
   shader_display.end();
-  
 
 }
 
@@ -309,7 +312,7 @@ function updateRD(){
     if(mouseIsPressed){
       noStroke();
       fill(0,255,0);
-      ellipse(mouseX, mouseY, 80, 80);
+      ellipse(mouseX, mouseY, 50, 50);
     }
     // >
     
@@ -326,6 +329,211 @@ function updateRD(){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+// p5 bugfixes
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+p5.Vector.cross = function cross(a, b, dst) {
+  dst = dst || new p5.Vector();
+  dst.x = a.y * b.z - a.z * b.y;
+  dst.y = a.z * b.x - a.x * b.z;
+  dst.z = a.x * b.y - a.y * b.x;
+  return dst;
+};
+
+
+p5.Vector.prototype.normalize = function normalize() {
+  
+  var x = this.x;
+  var y = this.y;
+  var z = this.z;
+  
+  var mag = x * x + y * y + z * z; // squared
+  if(mag !== 1.0 && mag !== 0.0){
+    mag = 1.0 / Math.sqrt(mag);
+    x *= mag;
+    y *= mag;
+    z *= mag;
+  }
+  
+  this.x = x;
+  this.y = y;
+  this.z = z;
+  return this;
+};
+
+
+
+
+p5.Geometry.prototype._getFaceNormal = function(faceId, normal) {
+ 
+  // face data
+  var face = this.faces[faceId];
+  var vA = this.vertices[face[0]];
+  var vB = this.vertices[face[1]];
+  var vC = this.vertices[face[2]];
+  
+  // reuse existing 
+  this.tmp_ab = this.tmp_ab || new p5.Vector();
+  this.tmp_ac = this.tmp_ac || new p5.Vector();
+  normal = normal || new p5.Vector();
+  
+  // base
+  var ab = this.tmp_ab.set(vB).sub(vA);
+  var ac = this.tmp_ac.set(vC).sub(vA);
+
+  // check for zero length
+  var mag_ab = ab.magSq();
+  var mag_ac = ac.magSq();
+  if(mag_ab === 0.0 || mag_ac === 0.0){
+    return normal.set(0,0,0);
+  }
+  
+  // cross product
+  normal = p5.Vector.cross(ab, ac, normal);
+
+  // length adjustment
+  // var mag = normal.mag();
+  // mag_ab = Math.sqrt(mag_ab);
+  // mag_ac = Math.sqrt(mag_ac);
+  // var alpha = mag / (mag_ab * mag_ac);
+  // normal.mult(Math.asin(alpha) / mag);
+  
+  // normalize
+  normal.normalize();
+  
+  return normal;
+};
+
+
+
+p5.Geometry.prototype.computeNormals = function() {
+  
+  var verts = this.vertices;
+  var faces = this.faces;
+
+  var num_verts = verts.length;
+  var num_faces = faces.length;
+  
+  
+  // 1) init list of face id's for each vertex
+  this.vert_facelist = this.vert_facelist || [];
+  for (var vi = 0; vi < num_verts; vi++) {
+    this.vert_facelist[vi] = [];
+  }
+  
+  // 2) precompute face-normals
+  //    create list of face id's for each vertex
+  this.facenormals = this.facenormals || [];
+  for (var fi = 0; fi < num_faces; fi++) {
+    this.facenormals[fi] = this._getFaceNormal(fi, this.facenormals[fi]);
+    this.vert_facelist[faces[fi][0]].push(fi);
+    this.vert_facelist[faces[fi][1]].push(fi);
+    this.vert_facelist[faces[fi][2]].push(fi);
+  }
+  
+  // create vertex normal by averaging adjacent face-normals.
+  this.vertexNormals = this.vertexNormals || [];
+  for (var vi = 0; vi < num_verts; vi++) {
+
+    // reuse existing
+    var normal = this.vertexNormals[vi];
+    if(normal){
+      normal.set(0,0,0);
+    } else {
+      normal = new p5.Vector();
+    }
+    // iterate through all adjacent faces and sum up face normals
+    var face_ids = this.vert_facelist[vi];
+    var num_face_ids = face_ids.length;
+    for (var k = 0; k < num_face_ids; k++) {
+      var fi = face_ids[k];
+      normal.add(this.facenormals[fi]);
+    }
+    // each normal was normalized, so we can ust divide by the count
+    normal.mult(1.0 / num_face_ids); 
+    // normal.normalize();
+    this.vertexNormals[vi] = normal;
+  }
+  
+  return this;
+};
+
+
+
+
+
+p5.RendererGL.prototype._edgesToVertices = function(geom) {
+
+  var verts = geom.vertices;
+  var edges = geom.edges;
+  
+  var edge;
+  var vA, vB, viA, viB;
+  var dx, dy, dz, dd, dn, dp;
+  
+  geom.lineVertices = [];
+  geom.lineNormals = [];
+  
+  for (var i = 0; i < edges.length; i++) {
+    edge = edges[i];
+    
+    // edge vertex indices
+    viA = edge[0];
+    viB = edge[1];
+    
+    // edge vertices
+    vA = verts[viA];
+    vB = verts[viB];
+
+    // dir
+    dx = vB.x - vA.x;
+    dy = vB.y - vA.y;
+    dz = vB.z - vA.z;
+    
+    // normalize
+    dd = dx*dx + dy*dy + dz*dz;
+    // ignore length 0.0 or 1.0
+    if(dd !== 1.0 && dd !== 0.0){
+      dd = 1.0 / Math.sqrt(dd);
+      dx *= dd;
+      dy *= dd;
+      dz *= dd;
+    }
+    
+    vA = [vA.x, vA.y, vA.z];
+    vB = [vB.x, vB.y, vB.z];
+    dp = [dx, dy, dz, +1];
+    dn = [dx, dy, dz, -1];
+    
+    geom.lineNormals .push(dp, dn, dp, dp, dn, dn);
+    geom.lineVertices.push(vA, vA, vB, vB, vA, vB);
+  }
+  
+};
 
 
 
